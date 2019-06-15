c794a29e5e68680e5009ce23214d0099a031030c
Bi'g'LeG <705668684@qq.com>
2019-06-15 17:27:23 +0800

    第一次添加get_idea

---
+++ git_first/src/cn/itcast/first_git.java
@@ -0,0 +1,15 @@
+package cn.itcast;
+
+public class first_git {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+
+}
